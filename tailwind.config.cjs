e3102e3c417665d36e812daf0168160ea5debd7e
wch3306 <zklco06685@126.com>
2025-08-22 19:33:44 +0800

    修改字体

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["MiSans"],
+        sans: ["Kai", "Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
     },
   },
